825c9b8e03633200ae1403580c8f285f92c8a08d
tony2tones <anthony.marque@gmail.com>
2018-05-24 16:06:20 +0200

    trying to setup React from Scratch

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,9 @@ var webpack = require('webpack');
 
 module.exports = {
     devtool: 'cheap-module-eval-source-map',
-    entry: [
-        './src/index'
-    ],
+    entry: {
+        app:'./src/index',
+    },
     output: {
         path: path.join(__dirname, 'dist'),
         filename: 'bundle.js',
@@ -16,8 +16,8 @@ module.exports = {
     ],
     module: {
         loaders: [{
-            test:/\.js/, 
-            loader: 'babel',
+            test:/\.js/ + /\.jsx?$/, 
+            loader: 'babel-loader',
             include: path.join(__dirname, 'src'),
         }]
     }
