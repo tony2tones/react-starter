649590d38ef4af0fb0a736c3dff11c329d2ebc4a
tony2tones <anthony.marque@gmail.com>
2018-05-28 16:05:29 +0200

    still not working

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default class App extends React.Component {
+export default class App extends Component {
     render() {
         return (
             <div>
                 <h1>Allo</h1>
-            </div>
-        )
+            </div >
+        );
     }
+
 }
+
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -4,22 +4,30 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     devtool: 'cheap-module-eval-source-map',
-    entry: {
-        app:'./src/index',
-    },
+    entry: './src/index',
     output: {
         path: path.join(__dirname, 'dist'),
-        filename: 'bundle.js',
-        publicPath: '/static/'
+        filename: 'bundle.js'
+    },
+    devServer: {
+        inline: true,
+        port: 8008
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/ + /\.jsx$/,
+                exclude: '/node_modules/',
+                use: {
+                    loader: 'babel-loader',
+                }
+            }
+        ]
     },
     plugins: [
-        new HtmlWebpackPlugin(['app'])
-    ],
-    module: {
-        loaders: [{
-            test:/\.js/ + /\.jsx?$/, 
-            loader: 'babel-loader',
-            include: path.join(__dirname, 'src'),
-        }]
-    }
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: "./index.html"
+        })
+    ]
 }
